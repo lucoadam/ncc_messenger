843e66c4b8b844b1eacbf1c8eae99116c4aaed5c
Alish Dahal <lucoadam@gmail.com>
2021-02-15 09:07:12 +0545

    updated

--- filter.sh
+++ filter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="icekachandra@gmail.com"
+OLD_EMAIL="53084585+iceka@users.noreply.github.com"
 CORRECT_NAME="Lucao Adam"
 CORRECT_EMAIL="lucoadam@gmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
